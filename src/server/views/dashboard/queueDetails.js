ed35d96b184b98d18689cd4b6e0dba95a1e80c62
Nikita Karpenkov <nikita.karpenkov@gmail.com>
2018-09-11 19:01:14 +0300

    error handling for queue details view

--- src/server/views/dashboard/queueDetails.js
+++ src/server/views/dashboard/queueDetails.js
@@ -1,27 +1,31 @@
 const _ = require('lodash');
 const QueueHelpers = require('../helpers/queueHelpers');
 
-async function handler(req, res) {
-  const {queueName, queueHost} = req.params;
-  const {Queues} = req.app.locals;
-  const queue = await Queues.get(queueName, queueHost);
-  if (!queue) return res.status(404).render('dashboard/templates/queueNotFound', {queueName, queueHost});
+async function handler(req, res, next) {
+  try {
+    const {queueName, queueHost} = req.params;
+    const {Queues} = req.app.locals;
+    const queue = await Queues.get(queueName, queueHost);
+    if (!queue) return res.status(404).render('dashboard/templates/queueNotFound', {queueName, queueHost});
 
-  let jobCounts;
-  if (queue.IS_BEE) {
-    jobCounts = await queue.checkHealth();
-    delete jobCounts.newestJob;
-  } else {
-    jobCounts = await queue.getJobCounts();
+    let jobCounts;
+    if (queue.IS_BEE) {
+      jobCounts = await queue.checkHealth();
+      delete jobCounts.newestJob;
+    } else {
+      jobCounts = await queue.getJobCounts();
+    }
+    const stats = await QueueHelpers.getStats(queue);
+
+    return res.render('dashboard/templates/queueDetails', {
+      queueName,
+      queueHost,
+      jobCounts,
+      stats
+    });
+  } catch (e) {
+    next(e);
   }
-  const stats = await QueueHelpers.getStats(queue);
-
-  return res.render('dashboard/templates/queueDetails', {
-    queueName,
-    queueHost,
-    jobCounts,
-    stats
-  });
 }
 
 module.exports = handler;
